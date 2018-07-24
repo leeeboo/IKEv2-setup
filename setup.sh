83fbcee5b39e457f60ceec6ea05180df566e290b
Albert Lee <lb13810398408@gmail.com>
2018-07-24 11:14:06 +0800

    modify

--- setup.sh
+++ setup.sh
@@ -113,6 +113,8 @@ iptables -A INPUT -m state --state INVALID -j DROP
 iptables -I INPUT -i $ETH0ORSIMILAR -m state --state NEW -m recent --set
 iptables -I INPUT -i $ETH0ORSIMILAR -m state --state NEW -m recent --update --seconds 60 --hitcount 12 -j DROP
 
+# accept (non-standard) SSH
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
 # VPN
 
